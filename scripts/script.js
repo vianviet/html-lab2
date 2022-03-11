dec93d0d0c6bd4e2a4f1ca1240bfab6ed02b44fc
vianviet <vianviet2502@gmail.com>
2022-03-11 11:45:57 +0700

    chapter 11

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ Date: 11/03/2022
 */
 
 //Hamburger menu function
-function menu() {
+function hamburger() {
     var navlinks = document.getElementById("nav-links");
     var menuicon = document.getElementById("icon");
     if (navlinks.style.display === "block") {
